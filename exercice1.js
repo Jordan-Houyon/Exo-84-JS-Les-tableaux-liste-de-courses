31323b940d4819efa48fe36862c6c73329ebc175
upto <jordanhouyon@outlook.fr>
2022-12-02 15:33:11 +0100

    fini

--- exercice1.js
+++ exercice1.js
@@ -12,6 +12,31 @@ let articles = [
 
 // Votre code à partir d'ici :
 
+let Course=document.getElementById("listeCourse").innerHTML+=articles;
+
+let addButton = document.getElementById('add')
+let delButton = document.getElementById('del')
+let input = document.getElementById('input')
+
+
+for (let obj of articles){
+    let list = document.createElement('li')
+    list.innerHTML = obj;
+    ul.appendChild(list)
+    list.addEventListener("click", function (){
+        list.remove()
+    })
+
+}
+addButton.addEventListener("click", function (){
+    let newLi = document.createElement('li')
+    newLi.innerHTML = input.value;
+    ul.appendChild(newLi)
+    delButton.addEventListener("click", function (){
+        newLi.remove()
+    })
+})
+
 
 
 
